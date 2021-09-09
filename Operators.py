dadd44dafec775a89995332dae6740decf18baa1
Haykuhidan <90092758+Haykuhidan@users.noreply.github.com>
2021-09-10 01:04:20 +0400

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 32,
+   "id": "40865028",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The hypotenuse of the triangle ABC= 5.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "import math\n",
+    "AB = 3\n",
+    "AC = 4\n",
+    "BC = math.sqrt(AB**2 +AC**2)\n",
+    "print('The hypotenuse of the triangle ABC=', BC)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ variables.py
@@ -0,0 +1,125 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "604ea830",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "str"
+      ]
+     },
+     "execution_count": 11,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "course = str('Intro to Python')\n",
+    "type (course)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "5c081fda",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "student = 'Haykuhi Danielyan'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "id": "91cf523c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "nickname = 'girl with green eyes'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "id": "0b73ac55",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "int"
+      ]
+     },
+     "execution_count": 14,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "grade = int(50)\n",
+    "type(grade)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "8239c1d5",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "pythonExpert = bool('true' or 'false')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 24,
+   "id": "4f9ce49f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "course: Intro to Python\n",
+      "student: Haykuhi Danielyan\n",
+      "nickname: girl with green eyes\n",
+      "grade: 50\n",
+      "pythonExpert: True\n"
+     ]
+    }
+   ],
+   "source": [
+    "print('course:',course)\n",
+    "print('student:', student)\n",
+    "print('nickname:', nickname)\n",
+    "print('grade:', grade)\n",
+    "print('pythonExpert:', pythonExpert)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
